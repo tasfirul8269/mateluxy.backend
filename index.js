561ca8b9a5d78705442178f48f8695177b311098
Tasfirul Haque <adef05675@gmail.com>
2025-05-04 07:01:40 +0600

    Listen

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ app.use((err, req, res, next) => {
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, '0.0.0.0', () => {
-    console.log('Server is running on port 3000');
+    console.log(`Server is running on  ${PORT}`);
 });
 
 
